c54871f6dc16e96a1eb1fb19eef564ca069bfa06
bNobo <benoit.rocco@gmail.com>
2025-07-14 18:54:25 +0200

    drop service worker cache to avoid weird bugs

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lunaretmap-cache-v1';
+const CACHE_NAME = 'lunaretmap-cache-v2';
 const urlsToCache = [
   '',
   'index.html',
@@ -17,5 +17,16 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
   );
 });
